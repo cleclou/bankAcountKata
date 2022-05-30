1e8efbca8b2c4a44417edd2786d12a35da3ef39e
Clémence LOKO <Clmence LOKO@host.docker.internal>
2022-05-30 17:59:36 +0200

    Création classe de test pour l'historique des transaction

---
+++ src/test/java/com/bank/account/kata/test/HistoryServiceTest.java
@@ -0,0 +1,37 @@
+package com.bank.account.kata.test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bank.account.kata.entity.Transactions;
+
+
+public class HistoryServiceTest {
+	
+	Transactions transactions;
+	List<Transactions> list = new ArrayList<>();
+	
+	@Before
+	public void setup() {
+		transactions = new Transactions(BigDecimal.valueOf(400), BigDecimal.valueOf(2500));
+		list.add(transactions);
+		transactions = new Transactions(BigDecimal.valueOf(50), BigDecimal.valueOf(2550));
+		list.add(transactions);
+	}
+	
+	@Test
+	public void return_list_transactions() {
+		int sizeMax = list.size();
+		assertThat(sizeMax, equalTo(2));
+		assertThat(list.get(sizeMax-1).getBalance(), equalTo(BigDecimal.valueOf(2550)));		
+	}
+
+}
