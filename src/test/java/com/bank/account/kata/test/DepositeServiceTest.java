17c1122006f07f784234cfb7eff11725896ea752
Clémence LOKO <Clmence LOKO@host.docker.internal>
2022-05-30 17:36:37 +0200

    Ajout deuxième test dépôt d'argent si utilisation des décimal

--- src/test/java/com/bank/account/kata/test/DepositeServiceTest.java
+++ src/test/java/com/bank/account/kata/test/DepositeServiceTest.java
@@ -7,18 +7,26 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class DepositeServiceTest {
-	
+
 	@Test
 	public void deposit_money_without_decimal_to_my_account() {
 		BigDecimal deposit = new BigDecimal(400);
 		BigDecimal myAccount = add(deposit);
 
 		assertThat(myAccount, equalTo(BigDecimal.valueOf(2500)));
-		
+
 	}
-	
+
+	@Test
+	public void deposit_money_with_decimal_to_my_account() {
+		BigDecimal deposit = BigDecimal.valueOf(400.87);
+		BigDecimal myAccount = add(deposit);
+
+		assertThat(myAccount, equalTo(BigDecimal.valueOf(2500.87)));
+	}
+
 	private BigDecimal add(BigDecimal deposit) {
-		BigDecimal myAccount = new BigDecimal(2100);
+		BigDecimal myAccount = BigDecimal.valueOf(2100);
 		return myAccount.add(deposit);
 	}
 
